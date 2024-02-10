f70f1141515660b76fbb924814dc953518dd0b56
Casper Zandbergen <info@casperzandbergen.nl>
2024-02-10 17:15:37 +0100

    bla

--- lua/dontforgit/init.lua
+++ lua/dontforgit/init.lua
@@ -3,27 +3,34 @@ local M = {}
 M.default_config = {
     notify_git_failed = true,
     git_command = "git",
+    compact = true,
 }
 
 M.setup = function (opts)
     local config = vim.tbl_extend("force", M.default_config, opts or {})
 
+    local function has_pending_changes()
+        local ok, ret = pcall(vim.fn.systemlist, { config.git_command, "status", "-s" })
+        ok = ok and vim.v.shell_error == 0
+        if not ok and config.notify_git_failed then
+            vim.ui.input({ prompt = "failed to get git status" }, function() end)
+        end
+
+        return ok and #ret > 0
+    end
+
     vim.api.nvim_create_autocmd({"VimLeavePre"}, {
         group = vim.api.nvim_create_augroup('amzd/dontforgit', {}),
         pattern = "*",
         callback = function ()
-            local ok, ret = pcall(vim.fn.systemlist, { config.git_command, "status", "-s" })
-            ok = ok and vim.v.shell_error == 0
-            if not ok and config.notify_git_failed then
-                vim.ui.input({ prompt = "Failed to get git status" }, function() end)
-            end
-            -- there's nothing to commit: skip
-            if not ok or #ret == 0 then return end
+            if not has_pending_changes() then return end
 
-            local git_status = "!" .. config.git_command .. " status"
+            local git_status = "!" .. config.git_command .. " status" .. (config.compact and " -s" or "")
             local input = git_status
             while input ~= nil do
                 pcall(vim.cmd, input)
+
+                -- if not has_pending_changes() then break end
 
                 vim.ui.input({ prompt = ":" }, function (new_input)
                     if new_input == "" then
